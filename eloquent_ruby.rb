28c67c5bc4828ad52aba00da4e4d1341450150b8
adam-conway <adam.n.conway@gmail.com>
2018-01-23 18:55:48 -0700

    Finished step 1:T Taking advantage of Ruby's Smart Collections

--- eloquent_ruby.rb
+++ eloquent_ruby.rb
@@ -13,4 +13,10 @@ puts capitalized_animals
 puts capitalized_animals.join(", ")
 
 #Dangers of ! methods
-#
+#Typically, methods that have a bang (!) are methods that will actually change
+#your collection rather than just return a modified copy of it.
+
+#The advantages to this is that you can use it to make changes to collections
+#with a lot less code. The dangers are obviously that you can do this without
+#meaning to. You should use one when reasonably sure nothing (or nobody) can
+#break something on accident.
